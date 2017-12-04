c44cbc0647d59d34afddbe0b3f70a57a29162239
dragonet_d <Dragonet-D@163.com>
2017-12-04 18:01:08 +0800

    Aaaa

--- miaov-ract/精品案例/my-app/src/app.js
+++ miaov-ract/精品案例/my-app/src/app.js
@@ -4,16 +4,26 @@ import ReactDOM from 'react-dom';
 import './common/style/goodsSort.css';
 import goodsSortData from './common/data/goodsSortData';
 
-ReactDOM.render(
-  <div id="wrap">
-    <section id="section">
-      <nav id="choose">
-        你的选择是:
-      </nav>
-      <ul id="type">
 
-      </ul>
-    </section>
-  </div>,
+class App extends React.Component {
+  render() {
+
+    return (
+      <div id="wrap">
+        <section id="section">
+          <nav id="choose">
+            你的选择是:
+          </nav>
+          <ul id="type">
+
+          </ul>
+        </section>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
   document.querySelector('#root')
 );
