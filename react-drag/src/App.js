a74332b7f40c5e20291e0bbb8348114f590a672d
Dragonet_D <Dragonet_D@163.com>
2018-03-12 20:29:15 +0800

    aaa

--- react-drag/src/App.js
+++ react-drag/src/App.js
@@ -1,19 +1,14 @@
 import React, {Component} from 'react';
 import HTML5Backend from 'react-dnd-html5-backend';
 import {DragDropContext} from 'react-dnd';
-// import Test1 from './components/test/test1';
-// import Test2 from './components/test/test2';
-// import DustbinMultipleTargets from "./components/Multiple Targets";
-import DustbinSingleTarget from "./components/Single Target";
+import Board from './components/board/board';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          {/*<Test1 text="你好,世界"/>*/}
-          {/*<Test2 text="Hello World"/>*/}
-          {/*<DustbinMultipleTargets/>*/}
-          <DustbinSingleTarget/>
+          <Board knightPosition={[7, 4]}/>
         </div>
     );
   }
---
+++ react-drag/src/components/knight/knight.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Knight extends Component {
+  render() {
+    return (
+        <div style={{color: '#fff'}}>
+          马马马<br/>马马马<br/>马马马
+        </div>
+    );
+  }
+}
