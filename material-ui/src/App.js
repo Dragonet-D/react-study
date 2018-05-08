daed1ac88eb02c88991164f725d1eb1402726298
dragonet_d <Dragonet_D@163.com>
2018-05-08 21:18:53 +0800

    aaa

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import IconButtonExampleComplex from './components/iconButton';
+import TableExampleComplex from './components/table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <IconButtonExampleComplex/>
+        <div style={{display: 'none'}}>
+          <IconButtonExampleComplex/>
+        </div>
+        <TableExampleComplex/>
       </div>
     );
   }
