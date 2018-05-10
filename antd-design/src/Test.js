bbdc26ca26842d48ddbc2b28ff6477e978523fe2
dragonet_d <Dragonet_D@163.com>
2018-05-10 22:44:56 +0800

    aaa

--- antd-design/src/Test.js
+++ antd-design/src/Test.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Button  from 'antd/lib/button';
 import styles from './Test.less';
 
 export default class Test extends Component{
   render() {
     return(
-      <div className={styles.test}>123</div>
+      <div className={styles.test}>
+        <Button>123</Button>
+      </div>
     )
   }
 }
