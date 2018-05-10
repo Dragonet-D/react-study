98d5331b6deed3e2f46c3b93decc2cccdeba1bd9
dragonet_d <Dragonet_D@163.com>
2018-05-10 22:19:01 +0800

    AaaA

---
+++ antd-design/src/Test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './Test.less';
+
+export default class Test extends Component{
+  render() {
+    return(
+      <div className={styles.test}>123</div>
+    )
+  }
+}
