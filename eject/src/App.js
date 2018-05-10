a5af3647236bf151856d552eea83b988f6ea419b
dragonet_d <Dragonet_D@163.com>
2018-05-11 00:27:04 +0800

    aa

--- eject/src/App.js
+++ eject/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,7 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button>123</Button>
       </div>
     );
   }
