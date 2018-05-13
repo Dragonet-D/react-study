b52b09d16ec16c93fb643fc1044c1c597db2b1d7
dragonet_d <Dragonet_D@163.com>
2018-05-13 14:49:43 +0800

    aaa

--- antd-design/src/App.js
+++ antd-design/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Test from './Test';
-import logo from './logo.svg';
+import IndexList from './components/List/Index';
+import IndexTable from './components/Table/Index';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Test/>
+        <IndexList/>
+        <IndexTable/>
       </div>
     );
   }
---
+++ antd-design/src/components/List/Index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { List } from 'antd';
+
+const dataSource = [{
+  key: '1',
+  name: '胡彦斌',
+  age: 32,
+  address: '西湖区湖底公园1号'
+}, {
+  key: '2',
+  name: '胡彦祖',
+  age: 42,
+  address: '西湖区湖底公园1号'
+}];
+
+const columns = [{
+  title: '姓名',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: '年龄',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: '住址',
+  dataIndex: 'address',
+  key: 'address',
+}];
+
+export default class IndexList extends Component{
+  render() {
+    return(
+      <div>
+        list
+      </div>
+    )
+  }
+}
