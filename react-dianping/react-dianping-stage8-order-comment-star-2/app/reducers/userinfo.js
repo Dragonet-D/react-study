496cd78fb083043aa13c07168eed09711a959db6
xiaolong <dragonet_d@163.com>
2018-05-31 23:03:02 +0800

    aaa

--- react-dianping/react-dianping-stage8-order-comment-star-2/app/reducers/userinfo.js
+++ react-dianping/react-dianping-stage8-order-comment-star-2/app/reducers/userinfo.js
@@ -5,7 +5,7 @@ const initialState = {}
 export default function userinfo (state = initialState, action) {
     switch (action.type) {
         case actionTypes.USERINFO_UPDATE:
-            return action.data
+            return action.data;
         default:
             return state
     }
