90abd77186d4868f7e03bfc33def3924c79f406b
xiaolong <dragonet_d@163.com>
2018-06-22 22:57:56 +0800

    openlayers second commit

--- openlayers/src/components/map/Map.jsx
+++ openlayers/src/components/map/Map.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import OlMap from "ol/map";
 
 export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.map = {};
+    }
     static defaultProps = {
         settings: {
             width: "100%",
@@ -9,14 +13,26 @@ export default class Map extends Component {
         },
         center: [103.784342, 1.352586],
         zoom: 12,
-        layerUrl: "https://map-{a-c}.onemap.sg/v3/Grey/{x}{y}.png"
+        layerUrl: "https://map-{a-c}.onemap.sg/v3/Grey/{x}{y}.png",
+        target: `map_${Math.random}`
     }
     componentDidMount() {
-        const {} = this.props;
+        const {
+            zoom,
+            center,
+            target,
+            layerUrl
+        } = this.props;
     }
     render() {
+        const {
+            zoom,
+            center,
+            target,
+            layerUrl
+        } = this.props;
         return (
-            <div>map</div>
+            <div id={target}/>
         )
     }
 }
