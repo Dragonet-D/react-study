d097e2f576a978bf84e66f286a3810b08d3c56c5
Deng XiaoLong <Dragonet_d@163.com>
2018-10-21 18:00:11 +0800

    leaflet map coordinate get

--- openlayers/src/components/leaflet/LeafletCoordinate.jsx
+++ openlayers/src/components/leaflet/LeafletCoordinate.jsx
@@ -6,9 +6,28 @@ console.log(a)
 export default class LeafletCoordinate extends Component{
   componentDidMount() {
     // [103.84435325860977, 1.38758564176523] [51.505, -0.09]
+    let aa = {x: 103.84435325860977 ,y: 1.38758564176523}
+    function _getLngLat(poi){
+      var lnglat = {};
+      lnglat.lng = poi.x/20037508.34*180;
+      var mmy = poi.y/20037508.34*180;
+      lnglat.lat = 180/Math.PI*(2*Math.atan(Math.exp(mmy*Math.PI/180))-Math.PI/2);
+      return lnglat;
+    }
+    function _getMercator(poi) {//[114.32894, 30.585748]
+      var mercator = {};
+      var earthRad = 6378137.0;
+      // console.log("mercator-poi",poi);
+      mercator.x = poi.x * Math.PI / 180 * earthRad;
+      var a = poi.y * Math.PI / 180;
+      mercator.y = earthRad / 2 * Math.log((1.0 + Math.sin(a)) / (1.0 - Math.sin(a)));
+      // console.log("mercator",mercator);
+      return mercator; //[12727039.383734727, 3579066.6894065146]
+    }
+    console.log(_getMercator(aa));
     const center = [103.84435325860977, 1.38758564176523];
     // const centeraa = OlProj.transform(center, 'WGS84' ,'EPSG:4326');
-    this.map = L.map('map').setView([39.897445, 116.331398], 10);
+    this.map = L.map('map').setView(_getLngLat(aa), 10);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
   };
   render() {
