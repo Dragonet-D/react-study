1c66172ffb7972fe4540068aa7770f98283be738
Deng XiaoLong <Dragonet_d@163.com>
2018-10-21 21:15:44 +0800

    add draw and icon in the map

--- openlayers/src/components/leaflet/LeafletCoordinate.jsx
+++ openlayers/src/components/leaflet/LeafletCoordinate.jsx
@@ -3,13 +3,11 @@ import L from "leaflet";
 import "leaflet-draw";
 import "./../../lib/leaflet.awesome-markers.css";
 import "./../../lib/material-icon-label";
+import "leaflet.label/src/Label";
 
 export default class LeafletCoordinate extends Component{
   componentDidMount() {
-    this.map = L.map('map').setView({
-      lat: 1.3697024,
-      lng: 103.8852096
-    }, 12);
+    this.map = L.map('map').setView(new L.LatLng(1.3697024, 103.8852096), 12);
     var token = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2o0OHZuY3MwMGo1cTMybGM4MTFrM2dxbCJ9.yCQe43DMRqobazKewlhi9w';
     var mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}@2x?access_token=' + token;
     L.tileLayer(mapboxUrl).addTo(this.map);
@@ -17,13 +15,8 @@ export default class LeafletCoordinate extends Component{
     var colors = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray'];
     var awesomeIcon = awesomeIcons[Math.floor(Math.random()*awesomeIcons.length)];
     var color = colors[Math.floor(Math.random()*colors.length)];
-    L.marker({
-      lat: 1.3697024,
-      lng: 103.8852096
-    }, {icon: L.MaterialIconWithLabel.icon({icon: awesomeIcon, prefix: 'fa', markerColor: "red"}) })
-      .addTo(this.map)
-      .bindPopup("<b>中国</b><br>安徽黄山.")
-      .openPopup();
+    L.marker(new L.LatLng(1.3697024, 103.8852096), {icon: L.MaterialIconWithLabel.icon({icon: awesomeIcon, prefix: 'fa', markerColor: "red"}) })
+      .bindPopup("<b>中国</b><br>安徽黄山.").addTo(this.map)
     const drawnItems = L.featureGroup().addTo(this.map);
     const editableLayers = new L.FeatureGroup();
     this.map.addLayer(editableLayers);
