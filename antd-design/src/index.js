dd48b0b740fca8dbc79c8d410978f66687896301
Deng XiaoLong <Dragonet_d@163.com>
2018-12-04 22:49:54 +0800

    react router study

--- antd-design/src/index.js
+++ antd-design/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={App}/>
+    </Switch>
+  </BrowserRouter>
+  ), document.getElementById('root'));
 registerServiceWorker();
