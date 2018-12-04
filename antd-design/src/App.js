50e17f727aad7a9f6741586058e7c5c9bfffd9d5
Deng XiaoLong <Dragonet_d@163.com>
2018-12-04 22:10:48 +0800

    react router study

--- antd-design/src/App.js
+++ antd-design/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import IndexList from './components/List/Index';
-import IndexTable from './components/Table/Index';
-import Sortable from './components/Drag/Sortable';
-import DropDownList from './components/DropDownList/DropDownList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <IndexList/>
-        <IndexTable/>
-        <Sortable/>
-        <DropDownList/>
+        123
       </div>
     );
   }
