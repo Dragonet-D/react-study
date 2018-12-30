55a6bffc2382490a7406579d153cf4fb67d2fdec
Deng XiaoLong <Dragonet_d@163.com>
2018-12-30 15:40:30 +0800

    typescript function study

--- reac-typescript/src/Components/Test/Funcs/utils.ts
+++ reac-typescript/src/Components/Test/Funcs/utils.ts
@@ -1,3 +1,26 @@
-export const test = () => {
-    return "hello typescript";
+const stringTest: string = "hello";
+const arr1: number[] = [1, 2, 3];
+const arr2: string[] = ["1", "2", "3"];
+
+export const test = (): string | string[] => {
+    return stringTest;
 };
+
+export const test1 = (): any[] => {
+    return [...arr1, ...arr2];
+};
+enum Enum {
+    Red = 1
+}
+
+console.log(Enum.Red);
+
+function f(x: number, y?: number) {
+    if(y) {
+        return x + y;
+    } else {
+        return x
+    }
+}
+
+f(1, 2);
