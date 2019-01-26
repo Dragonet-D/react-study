39a7a5eea5412f22c1f7d552e9df35605bdf796e
Deng XiaoLong <Dragonet_d@163.com>
2019-01-26 13:35:55 +0800

    leaflet with mapbox

--- leaflet/src/App.js
+++ leaflet/src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.map = {}
+  }
+
+  componentDidMount() {
+    this.map = L.map("map");
+    this.map.setView(new L.LatLng(1.3697024, 103.8852096), 12);
+    const token = 'pk.eyJ1IjoiZG9tb3JpdHoiLCJhIjoiY2o0OHZuY3MwMGo1cTMybGM4MTFrM2dxbCJ9.yCQe43DMRqobazKewlhi9w';
+    const mapboxUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}@2x?access_token=' + token;
+    L.tileLayer(mapboxUrl).addTo(this.map);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div id="map" style={{width: "100%", height: "700px"}}/>
     );
   }
 }
