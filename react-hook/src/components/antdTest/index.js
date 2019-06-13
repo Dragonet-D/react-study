21c12e68f9b89c196b471c18bea6f178f7bbb40d
Deng XiaoLong <Dragonet_d@163.com>
2019-06-13 21:25:01 +0800

    react hoc with input validate

--- react-hook/src/components/antdTest/index.js
+++ react-hook/src/components/antdTest/index.js
@@ -2,73 +2,178 @@ import React from "react";
 import "./style.scss";
 
 function AFormCreate(Comp) {
-    return class extends React.Component {
-        constructor(props) {
-            super(props);
-            this.options = {};
-            this.state = {};
-        }
-
-        handleChange = e => {
-            const {name, value} = e.target;
-            this.setState({
-                [name]: value
-            })
-        };
-
-        getFieldDec = (field, option, InputComp) => {
-            this.options[field] = option;
-            return (
-                <div>
-                    {
-                        React.cloneElement(InputComp, {
-                            name: field, // 控件name
-                            value: this.state[field] || "", // 控件值
-                            onChang: this.handleChange // change 事件
-                        })
-                    }
-                </div>
-            );
-        };
-
-        render() {
-            return (
-                <Comp
-                    {...this.props}
-                    getFieldDec={this.getFieldDec}
-                    value={this.state}
-                />
-            )
-        }
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.options = {};
+      this.state = {};
     }
+
+    // 处理表单项输入事件
+    handleChange = e => {
+      const {name, value} = e.target;
+      this.setState({
+        [name]: value
+      }, () => {
+        this.validateField(name);
+      });
+    };
+
+    validateField = field => {
+      const rules = this.options[field].rules;
+      const result = rules.some(rule => {
+        if (rule.required) {
+          if (!this.state[field]) {
+            // 校验失败
+            this.setState({
+              [field + 'Message']: rule.message
+            });
+            // 若有校验失败
+            return true;
+          }
+        }
+        return false;
+      });
+      // 没失败, 校验成功
+      if (!result) {
+        this.setState({
+          [field + 'Message']: ""
+        })
+      }
+      return !result;
+    };
+
+    // 校验所有字段
+    validate = cb => {
+      const result = Object.keys(this.options).map(field => {
+        return this.validateField(field)
+      }).every(item => item === true);
+      // 如果校验结果全部为true 校验成功
+      cb(result);
+    };
+
+    getFieldDec = (field, option, InputComp) => {
+      this.options[field] = option;
+      return (
+        <div>
+          {
+            React.cloneElement(InputComp, {
+              name: field, // 控件name
+              value: this.state[field] || "", // 控件值
+              onChange: this.handleChange, // change 事件
+              onFocus: this.handleFocus
+            })
+          }
+        </div>
+      );
+    };
+
+    // 焦点
+    handleFocus = (e) => {
+      const field = e.target.name;
+      this.setState({
+        [field + 'Focus']: true
+      });
+    };
+
+    // 判断是否被点过
+    isFieldTouched = field => {
+      return !!this.state[field + 'Focus']
+    };
+
+    getFieldError = field => this.state[field + "Message"];
+
+    render() {
+      return (
+        <Comp
+          {...this.props}
+          getFieldDec={this.getFieldDec}
+          value={this.state}
+          validate={this.validate}
+          isFieldTouched={this.isFieldTouched}
+          getFieldError={this.getFieldError}
+        />
+      )
+    }
+  }
+}
+
+class FormItem extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.children
+        }
+        {
+          this.props.validateStatus === "error" && (
+            <p>{this.props.help}</p>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+function Input(props) {
+  return (
+    <React.Fragment>
+      {
+        props.prefix
+      }
+      <input type="text" {...props}/>
+    </React.Fragment>
+  )
 }
 
 function AntdTest(props) {
+  const {
+    getFieldDec,
+    validate,
+    isFieldTouched,
+    getFieldError
+  } = props;
 
-    const onSubmit = () => {
+  const usernameError = isFieldTouched("username") && getFieldError("username");
+  const passwordError = isFieldTouched("pwd") && getFieldError("pwd");
+
+  const onSubmit = () => {
+    validate((isValid => {
+      if (isValid) {
         console.log(props.value);
-    };
+      }
+    }))
+  };
 
-    const {getFieldDec} = props;
-    return (
-        <div className="wrapper">
-            {
-                getFieldDec("username", {
-                    rules: [
-                        {required: true, message: "请输入用户名"}
-                    ]
-                }, <input type="text"/>)
-            }
-            {
-                getFieldDec("pwd", {
-                    rules: [
-                        {required: true, message: "请输入密码"}
-                    ]
-                }, <input type="password"/>)
-            }
-            <button onClick={onSubmit}>登录</button>
-        </div>
-    )
+  return (
+    <div className="wrapper">
+      <FormItem
+        validateStatus={usernameError ? "error" : ""}
+        help={usernameError || ""}
+      >
+        {
+          getFieldDec("username", {
+            rules: [
+              {required: true, message: "请输入用户名"}
+            ]
+          }, <Input type="text" prefix={"123"}/>)
+        }
+      </FormItem>
+      <FormItem
+        validateStatus={passwordError ? "error" : ""}
+        help={passwordError || ""}
+      >
+        {
+          getFieldDec("pwd", {
+            rules: [
+              {required: true, message: "请输入密码"}
+            ]
+          }, <Input type="password"/>)
+        }
+      </FormItem>
+      <button onClick={onSubmit}>登录</button>
+    </div>
+  )
 }
 
 export default AFormCreate(AntdTest);
