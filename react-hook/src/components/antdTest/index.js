36302a6be82ae9f0fce6dc1cdc811c2fd55f729a
dragonet-d <Dragonet_D@163.com>
2019-06-13 17:56:14 +0800

    use react hoc make the antd input come true

--- react-hook/src/components/antdTest/index.js
+++ react-hook/src/components/antdTest/index.js
@@ -1,14 +1,74 @@
 import React from "react";
 import "./style.scss";
 
+function AFormCreate(Comp) {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.options = {};
+            this.state = {};
+        }
+
+        handleChange = e => {
+            const {name, value} = e.target;
+            this.setState({
+                [name]: value
+            })
+        };
+
+        getFieldDec = (field, option, InputComp) => {
+            this.options[field] = option;
+            return (
+                <div>
+                    {
+                        React.cloneElement(InputComp, {
+                            name: field, // 控件name
+                            value: this.state[field] || "", // 控件值
+                            onChang: this.handleChange // change 事件
+                        })
+                    }
+                </div>
+            );
+        };
+
+        render() {
+            return (
+                <Comp
+                    {...this.props}
+                    getFieldDec={this.getFieldDec}
+                    value={this.state}
+                />
+            )
+        }
+    }
+}
+
 function AntdTest(props) {
+
+    const onSubmit = () => {
+        console.log(props.value);
+    };
+
+    const {getFieldDec} = props;
     return (
         <div className="wrapper">
-            <input type="text"/>
-            <input type="password"/>
-            <button>登录</button>
+            {
+                getFieldDec("username", {
+                    rules: [
+                        {required: true, message: "请输入用户名"}
+                    ]
+                }, <input type="text"/>)
+            }
+            {
+                getFieldDec("pwd", {
+                    rules: [
+                        {required: true, message: "请输入密码"}
+                    ]
+                }, <input type="password"/>)
+            }
+            <button onClick={onSubmit}>登录</button>
         </div>
     )
 }
 
-export default AntdTest;
+export default AFormCreate(AntdTest);
