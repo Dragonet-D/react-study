ec81f5cc2012ff651b49247b10fb05df24a3d4c4
Deng XiaoLong <Dragonet_d@163.com>
2019-06-15 10:31:08 +0800

    react router switch study

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ToDo from './components/hooks/ToDo';
 import Test from './components/hooks/Test';
 import LeafLet from './components/leaflet';
@@ -8,6 +8,7 @@ import Hoc1 from "./components/hoc";
 import ContextTest from "./components/contextWithHoc";
 import AntdTest from "./components/antdTest";
 import ReduxTest from "./components/reduxTest";
+import NoMatch from "./components/noMatch";
 
 import {Provider} from "react-redux";
 import store from "./store";
@@ -18,14 +19,17 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Route exact path="/" component={ToDo}/>
-        <Route path="/test" component={Test}/>
-        <Route path="/leaflet" component={LeafLet}/>
-        <Route path="/css" component={CssTest}/>
-        <Route path="/hoc" component={Hoc1}/>
-        <Route path="/context" component={ContextTest}/>
-        <Route path="/antd-test" component={AntdTest}/>
-        <Route path="/redux-test" component={ReduxTest}/>
+        <Switch>
+          <Route exact path="/" component={ToDo}/>
+          <Route path="/test" component={Test}/>
+          <Route path="/leaflet" component={LeafLet}/>
+          <Route path="/css" component={CssTest}/>
+          <Route path="/hoc" component={Hoc1}/>
+          <Route path="/context" component={ContextTest}/>
+          <Route path="/antd-test" component={AntdTest}/>
+          <Route path="/redux-test" component={ReduxTest}/>
+          <Route component={NoMatch}/>
+        </Switch>
       </Router>
     </Provider>
   );
