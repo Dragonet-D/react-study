e8b5cc85554220506566cbb4a5baf4542b240876
Deng XiaoLong <xiaolong.deng@ncs.com.sg>
2019-08-07 16:39:55 +0800

    react demo

--- react-hooks/src/components/hooks/Resizeable.jsx
+++ react-hooks/src/components/hooks/Resizeable.jsx
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Resizable } from "re-resizable";
-import Sector from './Sector';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Collapse from '@material-ui/core/Collapse';
+import Button from "@material-ui/core/Button";
 
-function Resizeable() {
-  const style = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    border: "solid 1px #ddd",
-    background: "#f0f0f0"
-  };
+const colorPickerWidth = 200;
+const colorPickerHeight = 200;
+function Resizeable(props) {
+  const { id } = props;
+  const [open, setOpen] = useState(false);
+  const [top, setTop] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const onClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const onOpen = useCallback(() => {
+    const btn = document.querySelector(`#color_picker_btn_${id}`),
+        client = btn.getBoundingClientRect(),
+        right = window.innerWidth - client.left,
+        clientLeft = client.left + client.width,
+        left = right > colorPickerWidth ? clientLeft : clientLeft - colorPickerWidth,
+        bottom = window.innerHeight - client.bottom,
+        clientTop = client.top + client.height,
+        top = bottom > colorPickerHeight ? clientTop : clientTop - colorPickerHeight;
+    setTop(top);
+    setLeft(left);
+    setOpen(true);
+  }, []);
   return (
       <>
-        <Resizable
-          style={style}
-          defaultSize={{
-            width: 200,
-            height: 200
-          }}
-        >
-          Sample with size
-        </Resizable>
-        <Sector
-          radius={200}
-          offsetAngle={90}
-          angle={150}
-        />
+        <Button onClick={onOpen} id={`color_picker_btn_${id}`}>Test</Button>
+        <ClickAwayListener onClickAway={onClose}>
+          <Collapse
+            in={open}
+            style={{
+              position: "fixed",
+              zIndex: 1301,
+              top,
+              left
+            }}
+          >
+            <Resizable
+              style={{
+                border: '1px solid pink',
+              }}
+              defaultSize={{
+                width: 200,
+                height: 200
+              }}
+              minWidth={200}
+              minHeight={200}
+            >
+              Sample with size
+            </Resizable>
+          </Collapse>
+        </ClickAwayListener>
       </>
   )
 }
+
+Resizeable.defaultProps = {
+  id: Date.now()
+};
 
 export default Resizeable;
