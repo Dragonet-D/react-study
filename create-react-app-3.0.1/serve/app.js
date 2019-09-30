8ec1f1e5e72ed2cbd5fdab65d68eb43890cfaa2d
Deng XiaoLong <Dragonet_d@163.com>
2019-09-30 22:52:20 +0800

    :recycle:

--- create-react-app-3.0.1/serve/app.js
+++ create-react-app-3.0.1/serve/app.js
@@ -2,12 +2,39 @@ const Koa = require('koa');
 const router = require('koa-router')();
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = new Koa();
 
-router.get('/', async (ctx, next) => {
+// user db model
+const UserScheme = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String
+});
 
+const User = mongoose.model('User', UserScheme);
+
+const user = {
+  username: 'xiaolong',
+  password: '123',
+  email: ''
+};
+
+const newUser = new User(user);
+newUser.save();
+
+router.get('/', async (ctx, next) => {
+  let val = null;
+  const data = await User.findOne({ username: 'xiaolong' });
+  console.log(data);
+  const result = {
+    code: 200,
+    response: data,
+    ts: 12345
+  };
+  ctx.response.body = result;
+  return result;
 });
 
 app.use(router.routes());
