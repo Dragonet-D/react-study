78b1f293a807b3178a8e19ffb030e1279d51619e
DengXiaoLong <dengxiaolong@deepexi.com>
2020-01-17 14:36:15 +0800

    :robot: react with typescript

---
+++ react-typescript/src/components/Test/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type IPorp = {
+  index: string
+}
+
+class Test extends React.Component<IPorp, any>{
+  render() {
+    return (
+        <div>123</div>
+    )
+  }
+}
+
+export default Test
