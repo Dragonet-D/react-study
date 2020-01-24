5a41d0364a45690c2ed74b2fdb2edb17db86c1cf
DengXiaoLong <dengxiaolong@deepexi.com>
2020-01-24 16:27:31 +0800

    :robot: typescript with react

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import Test from "./components/Test/index";
 
 class App extends React.Component {
+  public constructor(props: any) {
+    super(props);
+  }
+
   public render() {
     return (
       <div className="App">
-        <Test index={'123'}/>
+        <Test index='123' />
       </div>
     );
   }
--- react-typescript/src/components/Test/index.tsx
+++ react-typescript/src/components/Test/index.tsx
@@ -4,10 +4,36 @@ interface IPorp {
   index: string
 }
 
-class Test extends React.Component<IPorp, any>{
+interface IState {
+  count: number
+}
+
+class Test extends React.Component<IPorp, IState>{
+  public readonly state: Readonly<IState> = {
+    count: 0
+  }
+
+  public handleChange = () => {
+    this.setState((prev) => {
+      const {count} = prev
+      let tempCount = count
+      tempCount++
+      return {
+        count: tempCount
+      }
+    })
+  }
+
   public render() {
+    const {index} = this.props
+    const {count} = this.state
+
     return (
-        <div>{this.props.index}</div>
+        <React.Fragment>
+          <div>{index}</div>
+          <div>{count}</div>
+          <button onClick={this.handleChange}>test</button>
+        </React.Fragment>
     )
   }
 }
