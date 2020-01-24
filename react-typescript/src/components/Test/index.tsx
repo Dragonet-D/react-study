d345c9d414b83f4e7711f3f37b20ab68c05ec8dd
DengXiaoLong <dengxiaolong@deepexi.com>
2020-01-24 16:47:31 +0800

    :heart: react with typescript

--- react-typescript/src/components/Test/index.tsx
+++ react-typescript/src/components/Test/index.tsx
@@ -1,41 +1,41 @@
 import * as React from "react";
 
 interface IPorp {
-  index: string
+  index: string;
 }
 
 interface IState {
-  count: number
+  count: number;
 }
 
-class Test extends React.Component<IPorp, IState>{
+class Test extends React.Component<IPorp, IState> {
   public readonly state: Readonly<IState> = {
     count: 0
-  }
+  };
 
   public handleChange = () => {
-    this.setState((prev) => {
-      const {count} = prev
-      let tempCount = count
-      tempCount++
+    this.setState(prev => {
+      const { count } = prev;
+      let tempCount = count;
+      tempCount++;
       return {
         count: tempCount
-      }
-    })
-  }
+      };
+    });
+  };
 
   public render() {
-    const {index} = this.props
-    const {count} = this.state
+    const { index } = this.props;
+    const { count } = this.state;
 
     return (
-        <React.Fragment>
-          <div>{index}</div>
-          <div>{count}</div>
-          <button onClick={this.handleChange}>test</button>
-        </React.Fragment>
-    )
+      <React.Fragment>
+        <div>{index}</div>
+        <div>{count}</div>
+        <button onClick={this.handleChange}>test</button>
+      </React.Fragment>
+    );
   }
 }
 
-export default Test
+export default Test;
