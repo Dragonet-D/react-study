cf45d1ea648864e5d34920fe81de332da081f2ef
DengXiaoLong <dengxiaolong@deepexi.com>
2020-01-31 20:25:47 +0800

    :robot: typescript react study

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css'
 import StateTest from './components/03_State/App'
 import Test from './components/Test/index';
+import Callback from './components/04_Callback/App';
 
 class App extends React.Component {
   public constructor(props: any) {
@@ -18,9 +19,11 @@ class App extends React.Component {
     return (
       <Router>
         <Link to='/StateTest'>StateTest</Link>
+        <Link to='/Callback'>Callback</Link>
         <Switch>
           <Route exact path='/' component={TestCom} />
           <Route path='/StateTest' component={StateTest} />
+          <Route path='/Callback' component={Callback} />
         </Switch>
       </Router>
     );
