d314118b80d361bee1533a1e595a1908413a7151
dragonet-d <Dragonet_D@163.com>
2024-07-26 16:53:40 +0800

    feat: 添加demo

--- vite/src/pages/a/a.tsx
+++ vite/src/pages/a/a.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useMemo, useState } from 'react'
 import { Button } from 'antd'
 import B from './components/b'
+import C from './components/c'
 
 const A = () => {
   const [state, setState] = useState(0)
@@ -12,10 +13,13 @@ const A = () => {
 
   const value = [1, 2, 3]
 
+  const c = useCallback(() => <C />, [])
+
   return (
     <div>
       <Button onClick={() => setState(state + 1)}>Click {state}</Button>
       <B onClick={onClick} value={value} />
+      {c()}
     </div>
   )
 }
--- vite/src/pages/a/components/c.tsx
+++ vite/src/pages/a/components/c.tsx
@@ -1,5 +1,10 @@
+import { memo, useEffect } from 'react'
+
 const C = () => {
   console.log('C')
+  useEffect(() => {
+    console.log('123')
+  }, [])
 
   return <div>C</div>
 }
